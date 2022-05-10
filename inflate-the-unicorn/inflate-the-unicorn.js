964f80b4ca3427a692e7f52346c45b8f59604be8
Alvaro <alvarohudson@gmail.com>
2022-05-10 14:46:47 +1200

    Work up to step 3 of inflate the unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,17 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+//Variable for the images
+let unicorn = document.getElementsByClassName('inflate-an-image')
+
+function unicornClick() {
+  console.log('Hello')
+}
+
+//Loop to allow all the unicorn images variable to be accessed at anytime with a click
+for (let i = 0; i < unicorn.length; i++) {
+  unicorn[i].onclick = unicornClick
+}
