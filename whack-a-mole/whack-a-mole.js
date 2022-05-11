c5da06ace7caace13ba7801fd0dc80aa18433482
Alvaro <alvarohudson@gmail.com>
2022-05-12 11:58:37 +1200

    Complete 'Whack-A-Mole' challenge

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -6,8 +6,10 @@ console.log('Whack-a-Mole!')
 
 //Cells Variable
 let cellsArray = document.getElementsByTagName('td')
-let cellsArrayAlt = document.getElementById('table')
-console.log(cellsArrayAlt.rows[1].cells)
+let mole = document.createElement('img')
+mole.id = 'mole'
+mole.src = 'mole.png'
+let audio = new Audio('../whack-a-mole/whack-audio.wav')
 
 //Random number function
 function getRandomInt(min, max) {
@@ -16,13 +18,18 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-let randomIndex = getRandomInt(0, cellsArray.length - 1)
-let randomCell = cellsArray[randomIndex]
-
+//Function to make the mole show up randomly
 function showMole() {
-  let img = document.createElement('img')
-  img.id = 'mole'
-  img.src = 'mole.png'
-  randomCell.appendChild(img)
+  let randomIndex = getRandomInt(0, cellsArray.length - 1)
+  let randomCell = cellsArray[randomIndex]
+  randomCell.appendChild(mole)
+  console.log(randomCell)
 }
 showMole()
+
+function whackedMole(e) {
+  audio.play()
+  showMole()
+}
+
+mole.onclick = whackedMole
